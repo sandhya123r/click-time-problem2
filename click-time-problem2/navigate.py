d7fbf38a7be241e1cd1e53f3193f8ff26f11dc9b
Sandhya R <sandhyafeb1990@gmail.com>
2015-11-19 10:17:01 -0800

    11/18/2015 Application takes user options for mode of transport, snacks to be bought            and shows the list of options sorted by distance from the click time office.

--- click-time-problem2/navigate.py
+++ click-time-problem2/navigate.py
@@ -1,24 +1,132 @@
 import requests
 import json
+import datetime
+import sys
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
 
-def NavigateMe(origin):
-	destination = 'SanFrancisco'
+class places :
+	def __init__(self,name,place_id,rating,price_level,time_taken):
+		self.name=name
+		self.place_id=place_id
+		self.rating=rating
+		self.price_level=price_level
+		self.time_taken=time_taken
+	
+
+	
+def Summary(origin,destination,mode,mode_type):
+	if mode_type=='w':
+		transport='Walking'
+	if mode_type=='b' :
+		transport='bicycling'
+	if mode_type=='t'  :
+		transport='Transit'
+	key = 'AIzaSyCl_7phc2HQOSimmScmS09NW_A9dlQklqw'	
+	url='https://maps.googleapis.com/maps/api/directions/json?origin={0}&destination={1}&mode=bicycling&key={2}'.format(origin, destination,key)
+	r=requests.get(url)
+	x=json.loads(r.text)
+	legs=x['routes'][0]['legs']
+	steps=legs[0]['steps']
+	if mode=='summary':
+		return x['routes'][0]['summary']
+	if mode=='time' :
+		time=x['routes'][0]['legs'][0]['duration']['value']
+		return time 
+
+
+def GetOptions(origin,destination,mode):
+	food='coffee and donuts near '+destination
+	key = 'AIzaSyAAVTbGtg6q-4ioZF4gqM8pqXaLPxBILsM'
+	print("Coffee and Donut options around you are : \n" )
+	url='https://maps.googleapis.com/maps/api/place/textsearch/json?query={0}&key={1}'.format(food,key)
+	r=requests.get(url)
+	x=json.loads(r.text)
+	results=x['results']
+	place_objects=[]
+	print "Displaying some results \n"
+	for i,r in enumerate(results):
+		#print r['name']
+		name=r['name']	
+		place_id=r['place_id']
+		rating = r.get('rating', 'N/A')
+		price_level=r.get('price_level','N/A')
+		foo='place_id:'+r['place_id']
+		time_taken_b=Summary(foo,destination,'time',mode)
+		time_taken_a=Summary(origin,'place_id:'+place_id,'time',mode)
+		print time_taken_b," ",time_taken_a," ",time_taken_a+time_taken_b
+		time_taken=time_taken_a+time_taken_b
+		place_objects.append(places(name,place_id,rating,price_level,time_taken))
+		if (i+1)%10==0:
+			sorted(place_objects,key=lambda place:place.time_taken)
+			for j,place_object in enumerate(place_objects):
+				print j+1," ",place_object.name," ",place_object.rating," ", place_object.price_level,"",place_object.time_taken,"seconds"
+			more_options=raw_input("Do you want more options? Yes or No \n")
+			if more_options=='No' or more_options=='no':
+				final_option=raw_input("Which place would you like to go to ? \n")
+				break
+			
+		
+	
+
+		
+	#print i+1," : ", r['name'],time_taken,"minutes" , "Rating ", rating, price_level
+	#places.append((i,(place_id,r['name'])))
+	#if ((i+1)%10==0) :
+	#	more_options=raw_input("Do you want more options? Yes or No \n")
+	#	if more_options=='No' or more_options=='no':
+	#		final_option=raw_input("Which place would you like to go to ? \n")
+	#		return 0;
+			
+	#maintain a list of donut and coffee shops using their place_ids
+		
+	
+
+def NavigateMe(origin,transport,waypoints):
+	destination = '282 2nd Street 4th floor, San Francisco, CA 94105'
 	key = 'AIzaSyCl_7phc2HQOSimmScmS09NW_A9dlQklqw'
-	url='https://maps.googleapis.com/maps/api/directions/json?origin={0}&destination={1}&key={2}'.format(origin, destination, key)
+	GetOptions(destination)
+	url='https://maps.googleapis.com/maps/api/directions/json?origin={0}&mode={1}&destination={2}&key={3}'.format(origin, transport, destination, key)
 	r=requests.get(url)
 	x=json.loads(r.text)
 	legs=x['routes'][0]['legs']
 	steps=legs[0]['steps']
 	
-#	instructions=steps[0]['html_instructions']
-	print steps[0].keys()
+	#instructions=steps[0]['html_instructions']
+	#print steps[0].keys()
 	#print len(steps),steps[0].keys()
 	#print len(legs),legs[0].keys()
 	all_instructions = [x['html_instructions'] for x in steps]
 	print '\n'.join(all_instructions)
 
-#print "enter your location"
-#location=raw_input()
-#location=location.replace(" ","")
-location = 'SanJose'
-NavigateMe(location)
+
+location=raw_input("Enter your location\n")
+location=location.replace(" ","")
+destination = '282 2nd Street 4th floor, San Francisco, CA 94105'
+print "Best Times Summary\n"
+print "Walking via ",Summary(location,destination,'summary','w')," : ",str(datetime.timedelta(seconds=Summary(location,destination,'time','w')))," \n"
+print "Bicycling via ",Summary(location,destination,'summary','b')," : ",str(datetime.timedelta(seconds=Summary(location,destination,'time','b')))," \n"
+print "Transit by Public transport ",Summary(location,destination,'summary','t')," : ",str(datetime.timedelta(seconds=Summary(location,destination,'time','t')))," \n"
+
+option=raw_input("Which option do you prefer ? w for walking , b for bicycling , t for transit \n")
+opt_for_snacks=raw_input("Do you want to buy some coffee/donuts?y for yes,n for no \n")
+if opt_for_snacks=='y':
+	GetOptions(location,destination,option)
+	
+#if ans == 'y':
+#	transport='bicycling'
+#	NavigateMe(location,transport)
+#else :
+	#print "Walking from your location to office takes ",str(datetime.timedelta(seconds=CalculateDuration(location,'w')))," \n"
+	#print " Transit time with public transport takes ", str(datetime.timedelta(seconds=CalculateDuration(location,'t')))," \n"
+#	ans=raw_input("Do you want to walk or take public transport ? Enter w or t \n")
+#	if ans == 'w' :
+#		transport='walking'
+#		GetOptions(location,destination,ans)
+#		NavigateMe(location,transport)
+#	if ans=='t'   :
+#		transport='transit'
+#		GetOptions(location,destination,ans)
+#		NavigateMe(location,transport)
+
+
